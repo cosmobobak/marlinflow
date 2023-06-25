73f1aa2e2ffd4ffcc78d0e20bbeff820b28ee404
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2023-06-25 19:50:43 +0100

    add deconverter for binary format

---
+++ utils/src/data_to_txt.rs
@@ -0,0 +1,114 @@
+use std::io::{BufReader, BufWriter, Result, Write};
+use std::path::PathBuf;
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+};
+
+use bytemuck::Zeroable;
+use cozy_chess::Board;
+use marlinformat::PackedBoard;
+use structopt::StructOpt;
+
+/// Convert marlinformat to text data format.
+#[derive(StructOpt)]
+pub struct Options {
+    #[structopt(short, long)]
+    output: PathBuf,
+
+    data_file: PathBuf,
+
+    #[structopt(short, long)]
+    format: String,
+}
+
+trait Format {
+    fn write_into(
+        board: &Board,
+        cp: i16,
+        wdl: u8,
+        extra: u8,
+        output: &mut impl Write,
+    ) -> Result<()>;
+}
+
+struct MarlinformatLegacy;
+impl Format for MarlinformatLegacy {
+    fn write_into(
+        board: &Board,
+        cp: i16,
+        wdl: u8,
+        _extra: u8,
+        output: &mut impl Write,
+    ) -> Result<()> {
+        let wdl = match wdl {
+            0 => "0.0",
+            1 => "0.5",
+            2 => "1.0",
+            _ => unreachable!(),
+        };
+        writeln!(output, "{board} | {cp} | {wdl}")
+    }
+}
+struct Cudad;
+impl Format for Cudad {
+    fn write_into(
+        board: &Board,
+        cp: i16,
+        wdl: u8,
+        _extra: u8,
+        output: &mut impl Write,
+    ) -> Result<()> {
+        let wdl = match wdl {
+            0 => "0.0",
+            1 => "0.5",
+            2 => "1.0",
+            _ => unreachable!(),
+        };
+        writeln!(output, "{board} [{wdl}] {cp}")
+    }
+}
+
+pub fn run(options: Options) -> Result<()> {
+    let mut data = std::fs::File::open(options.data_file)?;
+    let size_bytes = data.seek(SeekFrom::End(0))?;
+    data.seek(SeekFrom::Start(0))?;
+    let count = size_bytes / std::mem::size_of::<PackedBoard>() as u64;
+
+    let mut reader = BufReader::new(data);
+
+    let mut output = BufWriter::new(File::create(options.output)?);
+
+    match options.format.as_str() {
+        "marlinformat-legacy" => {
+            conversion_loop::<MarlinformatLegacy>(count, &mut reader, &mut output)?
+        }
+        "cudad" => conversion_loop::<Cudad>(count, &mut reader, &mut output)?,
+        _ => panic!(
+            "unknown format {}, valid formats are marlinformat-legacy and cudad",
+            options.format
+        ),
+    }
+
+    Ok(())
+}
+
+fn conversion_loop<F: Format>(
+    count: u64,
+    reader: &mut impl Read,
+    output: &mut impl Write,
+) -> Result<()> {
+    print!("at {}/{}\r", 0, count);
+    for pos in 0..count {
+        let mut value = PackedBoard::zeroed();
+        reader.read_exact(bytemuck::bytes_of_mut(&mut value))?;
+        let (board, cp, wdl, extra) = value.unpack().expect("invalid board");
+        F::write_into(&board, cp, wdl, extra, output)?;
+        if pos % 1000 == 0 {
+            print!("at {}/{}\r", pos, count);
+        }
+    }
+    println!("at {}/{}", count, count);
+
+    Ok(())
+}

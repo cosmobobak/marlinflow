eb38e915b0f9fc305738121a05007cb2aa669e55
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2023-07-10 23:02:26 +0100

    hka

--- trainer/main.py
+++ trainer/main.py
@@ -153,7 +153,7 @@ def main():
 
     train_log = TrainLog(args.train_id)
 
-    model = SquaredPerspectiveNet(768).to(DEVICE)
+    model = HalfKANet(768).to(DEVICE)
     if args.resume is not None:
         model.load_state_dict(torch.load(args.resume))
 

1fbb3d2763eb298a49d26e1e7577bf96fe39b284
Cosmo Bobak <cosmobobak@gmail.com>
2023-08-22 22:30:47 +0100

    multicore!

--- utils/src/rescore.rs
+++ utils/src/rescore.rs
@@ -1,3 +1,4 @@
+use std::thread::ScopedJoinHandle;
 
 use anyhow::Context;
 use marlinformat::PackedBoard;
@@ -15,16 +16,16 @@ pub struct Options {
 
 unsafe fn mmap_into_slice_mut_with_lifetime<T>(mmap: &mut MmapMut) -> &mut [T] {
     let len = mmap.len() / std::mem::size_of::<T>();
-    std::slice::from_raw_parts_mut(
-        mmap.as_mut_ptr() as *mut T,
-        len,
-    )
+    std::slice::from_raw_parts_mut(mmap.as_mut_ptr() as *mut T, len)
 }
 
 pub fn run(options: Options) -> anyhow::Result<()> {
     // Initialize tablebases
     tablebases::probe::init(
-        options.tb_path.to_str().with_context(|| "Failed to convert tb_path to str")?,
+        options
+            .tb_path
+            .to_str()
+            .with_context(|| "Failed to convert tb_path to str")?,
     );
     // Open the dataset
     let dataset = std::fs::OpenOptions::new()
@@ -33,29 +34,48 @@ pub fn run(options: Options) -> anyhow::Result<()> {
         .open(options.dataset)
         .with_context(|| "Failed to open dataset")?;
     // mmap the dataset
-    let mut mmap = unsafe { 
-        memmap::MmapMut::map_mut(&dataset).with_context(|| "Failed to mmap dataset")?
-    };
+    let mut mmap =
+        unsafe { memmap::MmapMut::map_mut(&dataset).with_context(|| "Failed to mmap dataset")? };
     // Get a slice of the dataset
-    let positions = unsafe {
-        mmap_into_slice_mut_with_lifetime::<PackedBoard>(&mut mmap)
-    };
-    for position in positions.iter_mut() {
-        // unpack the position
-        let (board, eval, wdl, extra) = position.unpack().with_context(|| "Failed to unpack position")?;
-        // probe
-        if let Some(tb_wdl) = tablebases::probe::get_wdl_white(&board) {
-            let tb_wdl = match tb_wdl {
-                tablebases::probe::WDL::Win => 2,
-                tablebases::probe::WDL::Draw => 1,
-                tablebases::probe::WDL::Loss => 0,
-            };
-            if tb_wdl != wdl {
-                // update the position
-                *position = PackedBoard::pack(&board, eval, tb_wdl, extra);
-            }
+    let positions = unsafe { mmap_into_slice_mut_with_lifetime::<PackedBoard>(&mut mmap) };
+
+    // Determine threads to split work over
+    let threads = num_cpus::get();
+
+    // Split the work over the threads
+    std::thread::scope(|scope| {
+        let mut handles: Vec<ScopedJoinHandle<'_, Result<(), anyhow::Error>>> =
+            Vec::with_capacity(threads);
+        for chunk in positions.chunks_mut((positions.len() + threads - 1) / threads) {
+            // Spawn a thread
+            handles.push(scope.spawn(|| {
+                for position in chunk.iter_mut() {
+                    // unpack the position
+                    let (board, eval, wdl, extra) = position
+                        .unpack()
+                        .with_context(|| "Failed to unpack position")?;
+                    // probe
+                    if let Some(tb_wdl) = tablebases::probe::get_wdl_white(&board) {
+                        let tb_wdl = match tb_wdl {
+                            tablebases::probe::WDL::Win => 2,
+                            tablebases::probe::WDL::Draw => 1,
+                            tablebases::probe::WDL::Loss => 0,
+                        };
+                        if tb_wdl != wdl {
+                            // update the position
+                            *position = PackedBoard::pack(&board, eval, tb_wdl, extra);
+                        }
+                    }
+                }
+                Ok(())
+            }));
         }
-    }
+        // Wait for all threads to finish
+        for handle in handles {
+            handle.join().unwrap()?;
+        }
+        Ok::<(), anyhow::Error>(())
+    })?;
 
     Ok(())
 }

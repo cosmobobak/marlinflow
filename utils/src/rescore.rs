b36afbb56dba6256370547e02fc8b291f47a90c9
Cosmo Bobak <cosmobobak@gmail.com>
2023-08-24 19:14:17 +0100

    add loading bar and thread control

--- utils/src/rescore.rs
+++ utils/src/rescore.rs
@@ -1,4 +1,4 @@
-use std::thread::ScopedJoinHandle;
+use std::{thread::ScopedJoinHandle, sync::atomic::AtomicU64};
 
 use anyhow::Context;
 use marlinformat::PackedBoard;
@@ -12,6 +12,7 @@ use crate::tablebases;
 pub struct Options {
     dataset: std::path::PathBuf,
     tb_path: std::path::PathBuf,
+    threads: Option<usize>,
 }
 
 unsafe fn mmap_into_slice_mut_with_lifetime<T>(mmap: &mut MmapMut) -> &mut [T] {
@@ -40,16 +41,24 @@ pub fn run(options: Options) -> anyhow::Result<()> {
     let positions = unsafe { mmap_into_slice_mut_with_lifetime::<PackedBoard>(&mut mmap) };
 
     // Determine threads to split work over
-    let threads = num_cpus::get();
+    let max_threads = num_cpus::get();
+    let threads = options.threads.map(|t| t.min(max_threads)).unwrap_or(max_threads);
+    let positions_processed = AtomicU64::new(0);
+    let processed_ref = &positions_processed;
+    let total_positions = positions.len() as u64;
+    let digit_width = total_positions.to_string().len();
 
     // Split the work over the threads
     std::thread::scope(|scope| {
         let mut handles: Vec<ScopedJoinHandle<'_, Result<(), anyhow::Error>>> =
             Vec::with_capacity(threads);
-        for chunk in positions.chunks_mut((positions.len() + threads - 1) / threads) {
+        for (i, chunk) in positions.chunks_mut((positions.len() + threads - 1) / threads).enumerate() {
             // Spawn a thread
-            handles.push(scope.spawn(|| {
-                for position in chunk.iter_mut() {
+            handles.push(scope.spawn(move || {
+                if i == 0 {
+                    print!("Rescoring positions: {:w$}/{} (  0.00%)", 0, total_positions, w = digit_width);
+                }
+                for (p_idx, position) in chunk.iter_mut().enumerate() {
                     // unpack the position
                     let (board, eval, wdl, extra) = position
                         .unpack()
@@ -66,6 +75,17 @@ pub fn run(options: Options) -> anyhow::Result<()> {
                             *position = PackedBoard::pack(&board, eval, tb_wdl, extra);
                         }
                     }
+                    // update progress in batches -
+                    // all threads add to the counter, but only one prints.
+                    if p_idx % 1024 == 0 {
+                        processed_ref.fetch_add(1024, std::sync::atomic::Ordering::Relaxed);
+                        if i == 0 {
+                            // we're the main thread, print progress
+                            let processed = processed_ref.load(std::sync::atomic::Ordering::Relaxed);
+                            let percent = (processed as f64 / total_positions as f64) * 100.0;
+                            print!("\rRescoring positions: {:w$}/{} ({:6.2}%)", processed, total_positions, percent, w = digit_width);
+                        }
+                    }
                 }
                 Ok(())
             }));
@@ -76,6 +96,8 @@ pub fn run(options: Options) -> anyhow::Result<()> {
         }
         Ok::<(), anyhow::Error>(())
     })?;
+    // update the progress bar to 100%
+    println!("\rRescoring positions: {}/{} (100.00%)", total_positions, total_positions);
 
     Ok(())
 }
